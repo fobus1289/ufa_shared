e17e2052d28af6e2ff02fb156b708cc4880f517b
otabek <o_gulomov@uzinfocom.uz>
2024-03-18 15:56:48 +0500

    added verify to auth middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -11,6 +11,8 @@ type AuthMiddleware struct {
 	redisService redis.RedisService
 }
 
+const Unauthorized = "Unauthorized"
+
 func NewAuthMiddleware(jwtService jwtService.JwtService, redisService redis.RedisService) *AuthMiddleware {
 	return &AuthMiddleware{
 		jwtService:   jwtService,
@@ -20,13 +22,13 @@ func NewAuthMiddleware(jwtService jwtService.JwtService, redisService redis.Redi
 
 func (m *AuthMiddleware) Middleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
-		accessToken := ctx.Request().Header.Get("Authorization")
+		accessToken := ctx.Request().Header.Get(Unauthorized)
 		if accessToken == "" {
-			return echo.NewHTTPError(401, "Unauthorized")
+			return echo.NewHTTPError(401, Unauthorized)
 		}
 		_, err := m.jwtService.ExtractTokenMetadata(accessToken)
 		if err != nil {
-			return echo.NewHTTPError(401, "Unauthorized")
+			return echo.NewHTTPError(401, Unauthorized)
 		}
 		return next(ctx)
 	}
