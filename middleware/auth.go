db85da101ab5b0b23c43c3517e197a4d2bb6af02
otabek <o_gulomov@uzinfocom.uz>
2024-03-18 15:48:09 +0500

    added verify to auth middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,28 +1,30 @@
 package middleware
 
-import "github.com/labstack/echo/v4"
-
-type AccessToken string
-
-type AuthService interface {
-	VerifyToken(token AccessToken) error
-}
+import (
+	"github.com/fobus1289/ufa_shared/jwtService"
+	"github.com/fobus1289/ufa_shared/redis"
+	"github.com/labstack/echo/v4"
+)
 
 type AuthMiddleware struct {
-	authService AuthService
+	jwtService   jwtService.JwtService
+	redisService redis.RedisService
 }
 
-func NewAuthMiddleware(authService AuthService) *AuthMiddleware {
-	return &AuthMiddleware{authService: authService}
+func NewAuthMiddleware(jwtService jwtService.JwtService, redisService redis.RedisService) *AuthMiddleware {
+	return &AuthMiddleware{
+		jwtService:   jwtService,
+		redisService: redisService,
+	}
 }
 
 func (m *AuthMiddleware) Middleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(ctx echo.Context) error {
-		accessToken := AccessToken(ctx.Request().Header.Get("Authorization"))
+		accessToken := ctx.Request().Header.Get("Authorization")
 		if accessToken == "" {
 			return echo.NewHTTPError(401, "Unauthorized")
 		}
-		err := m.authService.VerifyToken(accessToken)
+		_, err := m.jwtService.ExtractTokenMetadata(accessToken)
 		if err != nil {
 			return echo.NewHTTPError(401, "Unauthorized")
 		}
