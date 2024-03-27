c8cb9f4796a1269675829dbb8f752b354d45e9f2
otabek <o_gulomov@uzinfocom.uz>
2024-03-27 12:06:50 +0500

    added paginate response initializer

--- http/response/paginate.go
+++ http/response/paginate.go
@@ -4,3 +4,10 @@ type PaginateResponse[T any] struct {
 	Total int `json:"total"`
 	Data  []T `json:"data"`
 }
+
+func NewPaginateResponse[T any](total int, data []T) *PaginateResponse[T] {
+	return &PaginateResponse[T]{
+		Total: total,
+		Data:  data,
+	}
+}
